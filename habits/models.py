3131e0ea561513b8a59d6497e292c49e5c95fe60
Ozodbek <ozodbekmaxammatov1@gmail.com>
2025-12-06 16:14:29 +0200

    Add habits: empty skeleton

--- habits/models.py
+++ habits/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Habit(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
