1899c96883d02b86d751cf7b7c05dd85728bd0d6
Ozodbek <ozodbekmaxammatov1@gmail.com>
2025-12-07 19:59:05 +0200

    App challenge update

--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import ListView, DetailView
 from django.utils import timezone
 from .models import Challenge
+from participation.models import Enrollment
+from tracking.models import Checkin
 
 
 class ChallengeList(ListView):
@@ -19,7 +21,6 @@ class ChallengeList(ListView):
             qs = qs.filter(start_date__gt=today)
         elif status == "past":
             qs = qs.filter(end_date__lt=today)
-        # default ordering from Meta
         return qs
 
     def get_context_data(self, **kwargs):
@@ -33,3 +34,25 @@ class ChallengeDetail(DetailView):
     template_name = "challenges/detail.html"
     slug_field = "slug"
     slug_url_kwarg = "slug"
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        user = self.request.user
+        enrollment = None
+        if user.is_authenticated:
+            enrollment = Enrollment.objects.filter(
+                user=user, challenge=self.object
+            ).first()
+        ctx["enrollment"] = enrollment
+        ctx["participants_count"] = self.object.enrollments.count()
+
+        if enrollment:
+            today = timezone.localdate()
+            qty_map = {
+                c.habit_id: c.quantity
+                for c in Checkin.objects.filter(enrollment=enrollment, done_at=today)
+            }
+            for chh in self.object.challenge_habits.select_related("habit"):
+                chh.today_qty = qty_map.get(chh.habit_id, 0)
+
+        return ctx
